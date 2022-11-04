e36e50fc2a47f7a01b9cb4bd658fd5e1bdb6bb60
SuaArraY <1313178@senati.pe>
2022-11-04 12:41:24 -0500

    diseño5

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -20,6 +20,8 @@
                 margin:0;
                 position:0;
                 font-family: 'Nunito', sans-serif;
+               
+                
             }
             .title{
                 font-size: 36px;
@@ -47,9 +49,11 @@
             .subtitle:hover{
                 color: #3F3B6C;
             }
+            
+            
         </style>
     </head>
-    <body class="antialiased">
+    <body class="antialiased ">
 
 
     
@@ -72,7 +76,7 @@
                 <div class="flex justify-center sm:justify-center
                  sm:pt-0">
                     
-                 <h1 class=title><a href="https://github.com/SuaArraY/ViernesLaravel.git" target="_blank" >YOSSEC 😎</a></h1> 
+                 <h1 class="title"><a href="https://github.com/SuaArraY/ViernesLaravel.git" target="_blank" >YOSSEC 😎</a></h1> 
                 
                 </div>
                 <hr class=linea>
@@ -80,7 +84,7 @@
                 <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
                     <div class="grid grid-cols-1 md:grid-cols-2">
                         <div class="p-6">
-                            <div class="flex items-center">
+                            <div class="flex items-center ">
                             
                             <!-- <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-8 h-8 text-gray-500"><path d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253"></path></svg> -->
                                 <div class=subtitle><a href="https://www.despegar.com.pe/vuelos/"  target="_blank ">✈ Agencia de Viajes</a></div>
@@ -93,7 +97,7 @@
                             </div>
                         </div>
 
-                        <div class="p-6 border-t border-gray-200 dark:border-gray-700 md:border-t-0 md:border-l">
+                        <div class="p-6 border-t border-gray-200 dark:border-gray-700 md:border-t-0 md:border-l" class=imagen>
                             <div class="flex items-center">
                                
                                 <div class=subtitle><a href="https://www.reservationstays.com/requests/H7vPWdLL5wNzkRBb_rB3ZQ/hotels/rW5LpnGg?expand_params=true "  target="_blank ">🏦 Hospedaje</a></div>
